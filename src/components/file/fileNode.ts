b9e9d46832adbd95b214ad8fc7fb9dbbc0779f29
ilmarch1 <ilmarch1@mts.ru>
2026-05-11 12:49:28 +0300

    feat: remove unused comment

--- src/components/file/fileNode.ts
+++ src/components/file/fileNode.ts
@@ -3,6 +3,5 @@ import { createTreeButton } from '../tree-button/treeButton';
 import fileIcon from '@/assets/file.svg'
 
 export function createFileNode(name: string, path: string, isActive?: boolean) {
-  // TODO: undefined - плохо, сделать параметры объектом
   return setupHashPusher(createTreeButton(name, fileIcon, 'file__button', isActive), path);
 }
